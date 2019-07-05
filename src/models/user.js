c35020c4e39ec424d19195a024871d009a049b24
Maksym Kucher <the.stewk@gmail.com>
2019-07-05 11:20:18 -0700

    - Added cascade task deletion after user's profile removal

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@ const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {jwtSecret} = require('../db/passwords');
+const Task = require('./task');
 
 
 const userSchema = new mongoose.Schema({
@@ -93,6 +94,12 @@ userSchema.pre('save', async function (next) {
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8);
     }
+    next();
+});
+
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({createdBy: user._id});
     next();
 });
 
